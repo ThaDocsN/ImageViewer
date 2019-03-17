490d27683c3561782b72c3202d4d7576fec68226
ThaDocIzN <rodan72@msn.com>
2019-03-17 13:40:19 -0500

    finish part 4

--- ImageViewer/app/src/main/java/com/thadocizn/imageviewer/MainActivity.java
+++ ImageViewer/app/src/main/java/com/thadocizn/imageviewer/MainActivity.java
@@ -1,26 +1,31 @@
 package com.thadocizn.imageviewer;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import butterknife.ButterKnife;
 
 import static com.thadocizn.imageviewer.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_main);
-        ButterKnife.bind(this);
+
+        linearLayout = findViewById(R.id.parentLayout);
+        linearLayout.addView(generateTextView("Charles", 1));
+
     }
 
     public TextView generateTextView(String text, int position) {
         TextView textView = new TextView(this);
         textView.setTextSize(18);
         textView.setId(position);
+        textView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
         textView.setText(text);
         return textView;
     }
