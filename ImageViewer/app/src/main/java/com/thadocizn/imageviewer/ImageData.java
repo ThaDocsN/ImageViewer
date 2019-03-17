63ee0e004ef266a45e55438191f5c129f0f38b11
ThaDocIzN <rodan72@msn.com>
2019-03-17 13:16:39 -0500

    added dependencies

--- ImageViewer/app/src/main/java/com/thadocizn/imageviewer/ImageData.java
+++ ImageViewer/app/src/main/java/com/thadocizn/imageviewer/ImageData.java
@@ -1,12 +1,10 @@
 package com.thadocizn.imageviewer;
 
-import androidx.databinding.BaseObservable;
-import androidx.databinding.Bindable;
 import android.net.Uri;
 
 import java.io.Serializable;
 
-public class ImageData extends BaseObservable implements Serializable {
+public class ImageData implements Serializable {
 
     private String name;
     private String uri;
@@ -16,24 +14,21 @@ public class ImageData extends BaseObservable implements Serializable {
         this.uri = uri.toString();
     }
 
-    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-        notifyPropertyChanged(BR.name);
+
     }
 
-    @Bindable
     public Uri getUri() {
         return Uri.parse(uri);
     }
 
     public void setUri(Uri uri) {
         this.uri = uri.toString();
-        notifyPropertyChanged(BR.uri);
     }
 
 
--- ImageViewer/app/src/main/java/com/thadocizn/imageviewer/MainActivity.java
+++ ImageViewer/app/src/main/java/com/thadocizn/imageviewer/MainActivity.java
@@ -1,15 +1,27 @@
 package com.thadocizn.imageviewer;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+
+import static com.thadocizn.imageviewer.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+        ButterKnife.bind(this);
+    }
 
-
+    public TextView generateTextView(String text, int position) {
+        TextView textView = new TextView(this);
+        textView.setTextSize(18);
+        textView.setId(position);
+        textView.setText(text);
+        return textView;
     }
 }
